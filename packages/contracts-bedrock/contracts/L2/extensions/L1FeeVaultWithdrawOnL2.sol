60186b0f96b5edd1ac49c5afa55401202e7ae7c7
180945 <180945@trustless.computer>
2024-03-05 14:46:33 +0700

    upgrade sequence vault

---
+++ packages/contracts-bedrock/contracts/L2/extensions/L1FeeVaultWithdrawOnL2.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { Semver } from "../../universal/Semver.sol";
+import { FeeVault } from "./FeeVaultWithdrawOnL2.sol";
+
+/**
+ * @custom:proxied
+ * @custom:predeploy 0x4200000000000000000000000000000000000011
+ * @title SequencerFeeVault
+ * @notice The SequencerFeeVault is the contract that holds any fees paid to the Sequencer during
+ *         transaction processing and block production.
+ */
+contract SequencerFeeVaultWithdrawOnL2 is FeeVault, Semver {
+    /**
+     * @custom:semver 1.1.0
+     * be able to withdraw fee when contract balance > 0
+     *
+     * @param _recipient Address that will receive the accumulated fees.
+     */
+    constructor(address _recipient) FeeVault(_recipient, 1) Semver(1, 1, 0) {}
+
+    /**
+     * @custom:legacy
+     * @notice Legacy getter for the recipient address.
+     *
+     * @return The recipient address.
+     */
+    function l1FeeWallet() public view returns (address) {
+        return RECIPIENT;
+    }
+}
