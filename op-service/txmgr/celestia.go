4d655680ca2729f3f248c5a491d2e70487e77056
Jack <jack@newbitcoincity.com>
2024-04-26 12:17:17 +0700

    add log debug

--- op-service/txmgr/celestia.go
+++ op-service/txmgr/celestia.go
@@ -49,7 +49,7 @@ func StoreBlob(url string, data []byte) (string, error) {
 		return "", err
 	}
 	if resp.Status != "200 OK" {
-		fmt.Println("error  when read body response from da code ", resp.Status, "body", body)
+		fmt.Println("error  when read body response from da code ", resp.Status, "body", string(body))
 		return "", fmt.Errorf("Error: %s", resp.Status)
 	}
 	// Read the response body
